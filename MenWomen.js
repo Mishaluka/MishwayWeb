0ae95a0910a5d2fab9b9b41c99378876af79d4c0
Mikhail <83475413+Mishaluka@users.noreply.github.com>
2021-12-08 14:31:20 -0500

    Delete MenWomen.js

--- MenWomen.js
+++
@@ -1,8 +0,0 @@
-var options = {
-    width: 400,
-    height: 250,
-    zoomWidth: 500,
-    offset: { vertical: 0, horizontal: 10 },
-    scale: 1.5
-}
-new ImageZoom(document.getElimentById("img-zoom"), options);
