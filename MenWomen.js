0e620555620ebce0224e14b993f3b177bdaaed12
Mikhail <83475413+Mishaluka@users.noreply.github.com>
2021-12-07 22:39:04 -0500

    Add files via upload

---
+++ MenWomen.js
@@ -0,0 +1,8 @@
+var options = {
+    width: 400,
+    height: 250,
+    zoomWidth: 500,
+    offset: { vertical: 0, horizontal: 10 },
+    scale: 1.5
+}
+new ImageZoom(document.getElimentById("img-zoom"), options);
